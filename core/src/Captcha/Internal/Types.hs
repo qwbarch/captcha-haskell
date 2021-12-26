7dcf03086f0bc6bbcf1ef67c22ed1df9aa027c75
qwbarch <edwardyang0410@gmail.com>
2021-12-26 17:44:14 -0500

    Refactor all records into a single file
    makeFieldsNoPrefix template generates a new typeclass for each field, but this makes a new typeclass for each file. This commit fixes it so that there's only one typeclass to prevent a cyclic dependency issue

--- core/src/Captcha/Internal/Types.hs
+++ core/src/Captcha/Internal/Types.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE StrictData #-}
@@ -11,6 +12,7 @@ import Control.Lens.TH (makeFieldsNoPrefix)
 import Data.Default (Default (def))
 import Data.Text (Text)
 import GHC.Generics (Generic)
+import Web.Cookie (Cookies)
 
 -- | 'Default' instance for 'Bool' is not defined by default.
 instance Default Bool where
@@ -43,3 +45,92 @@ data Proxy = Proxy
   deriving (Generic, Default)
 
 makeFieldsNoPrefix ''Proxy
+
+-- | Parameters for solving a captcha with text within an image.
+data ImageCaptcha a = ImageCaptcha
+  { -- | The captcha solver's API key.
+    _apiKey :: Text,
+    -- | The image, encoded in base-64.
+    _body :: Text
+  }
+  deriving (Generic, Default)
+
+makeFieldsNoPrefix ''ImageCaptcha
+
+-- | Parameters for solving a text captcha.
+data TextCaptcha a = TextCaptcha
+  { -- | The captcha solver's API key.
+    _apiKey :: Text,
+    -- | The text captcha to solve.
+    _body :: Text
+  }
+  deriving (Generic, Default)
+
+makeFieldsNoPrefix ''TextCaptcha
+
+-- | Parameters for solving Arkose Lab's FunCaptcha.
+data FunCaptcha a = FunCaptcha
+  { -- | The captcha solver's API key.
+    _apiKey :: Text,
+    -- | Url where the captcha is found.
+    _captchaUrl :: Text,
+    -- | FunCaptcha's __data-pkey__ value.
+    _captchaKey :: Text,
+    -- | FunCaptcha's __surl__ service url value.
+    _serviceUrl :: Maybe Text,
+    -- | User agent to be used when solving the captcha.
+    _userAgent :: Maybe Text,
+    -- | Proxy to be used when solving the captcha.
+    _proxy :: Maybe Proxy,
+    -- | Cookies to be used when solving the captcha.
+    _cookies :: Cookies
+  }
+  deriving (Generic, Default)
+
+makeFieldsNoPrefix ''FunCaptcha
+
+-- | Parameters for solving Google's reCAPTCHA v2.
+data ReCaptchaV2 a = ReCaptchaV2
+  { -- | The captcha solver's API key.
+    _apiKey :: Text,
+    -- | Url where the captcha is found.
+    _captchaUrl :: Text,
+    -- | reCAPTCHA v2's __data-sitekey__ value.
+    _captchaKey :: Text,
+    -- | reCAPTCHA's v2's __data-s__ value.
+    _dataS :: Maybe Text,
+    -- | Is the reCAPTCHA an __invisible__ or __normal__ captcha?
+    _invisible :: Bool,
+    -- | User agent to be used when solving the captcha.
+    _userAgent :: Maybe Text,
+    -- | Proxy to be used when solving the captcha.
+    _proxy :: Maybe Proxy,
+    -- | Cookies to be used when solving the captcha.
+    _cookies :: Cookies
+  }
+  deriving (Generic, Default)
+
+makeFieldsNoPrefix ''ReCaptchaV2
+
+-- | Parameters for solving Google's reCAPTCHA v3.
+data ReCaptchaV3 a = ReCaptchaV3
+  { -- | The captcha solver's API key.
+    _apiKey :: Text,
+    -- | Url where the captcha is found.
+    _captchaUrl :: Text,
+    -- | reCAPTCHA v3's __sitekey__ value.
+    _captchaKey :: Text,
+    -- | reCAPTCHA v3's minimum score.
+    _minScore :: Double,
+    -- | reCAPTCHA v3's __action__ value.
+    _action :: Maybe Text,
+    -- | User agent to be used when solving the captcha.
+    _userAgent :: Maybe Text,
+    -- | Proxy to be used when solving the captcha.
+    _proxy :: Maybe Proxy,
+    -- | Cookies to be used when solving the captcha.
+    _cookies :: Cookies
+  }
+  deriving (Generic, Default)
+
+makeFieldsNoPrefix ''ReCaptchaV3
