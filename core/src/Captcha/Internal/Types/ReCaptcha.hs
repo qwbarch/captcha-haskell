993869a6dd9dd8a5180051a94516e392877cff83
qwbarch <edwardyang0410@gmail.com>
2021-12-25 19:30:55 -0500

    Add reCAPTCHA v3

--- core/src/Captcha/Internal/Types/ReCaptcha.hs
+++ core/src/Captcha/Internal/Types/ReCaptcha.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE StrictData #-}
@@ -15,9 +16,9 @@ data ReCaptchaV2 = ReCaptchaV2
     _apiKey :: Text,
     -- | Url where the captcha is found.
     _captchaUrl :: Text,
-    -- | reCAPTCHA's __data-sitekey__ value.
+    -- | reCAPTCHA v2's __data-sitekey__ value.
     _captchaKey :: Text,
-    -- | reCAPTCHA's __data-s__ value.
+    -- | reCAPTCHA's v2's __data-s__ value.
     _dataS :: Maybe Text,
     -- | Is the reCAPTCHA an __invisible__ or __normal__ captcha?
     _invisible :: Bool,
@@ -26,3 +27,19 @@ data ReCaptchaV2 = ReCaptchaV2
   }
 
 makeFieldsNoPrefix ''ReCaptchaV2
+
+-- | Parameters for solving Google's reCAPTCHA v3.
+data ReCaptchaV3 = ReCaptchaV3
+  { -- | The captcha solver's API key.
+    _apiKey :: Text,
+    -- | Url where the captcha is found.
+    _captchaUrl :: Text,
+    -- | reCAPTCHA v3's __sitekey__ value.
+    _captchaKey :: Text,
+    -- | reCAPTCHA v3's minimum score.
+    _minScore :: Double,
+    -- | reCAPTCHA v3's __action__ value.
+    _action :: Maybe Text
+  }
+
+makeFieldsNoPrefix ''ReCaptchaV3
