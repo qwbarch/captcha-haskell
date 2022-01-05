dd69897fedb65b56c9a142bca52d513a566ccd79
qwbarch <edwardyang0410@gmail.com>
2022-01-04 19:12:32 -0500

    Remove top-level packages
    This will need to be redone eventually

--- capmonster/src/Captcha.hs
+++
@@ -1,9 +0,0 @@
-module Captcha
-  ( module Captcha.CapMonster.Internal.CapMonster,
-    module Captcha.CapMonster.Internal.Error,
-  )
-where
-
-import Captcha.CapMonster.Internal.CapMonster
-import Captcha.CapMonster.Internal.Error
-import Captcha.CapMonster.Internal.Types.Image ()
